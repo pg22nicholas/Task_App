b7e4ee9f5fb68c7a0d1d61b314b9ecf9f12eb80d
pg22nicholas <pg22nicholas@vfs.com>
2022-06-24 23:15:42 -0700

    data properly created when user first storing data

--- app/src/main/java/com/vfs/todoapp_final/models/FirebaseData.kt
+++ app/src/main/java/com/vfs/todoapp_final/models/FirebaseData.kt
@@ -18,34 +18,70 @@ object FirebaseData {
     val database = Firebase.database
 
     fun updateOnlineData(successCallback: (Boolean, String) -> Unit) {
-        database.getReference("users")
-            .child(LoginRepository.user!!.userId)
-            .child("data")
-            .setValue(Json.encodeToString(Data.categoryList))
+        database.getReference("users").child(LoginRepository.user!!.userId).addListenerForSingleValueEvent(object: ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+
+                if (snapshot.exists()) {
+                    database.getReference("users")
+                        .child(LoginRepository.user!!.userId)
+                        .child("data")
+                        .setValue(Json.encodeToString(Data.categoryList))
+                } else {
+                    database.getReference("users").child(LoginRepository.user!!.userId).setValue(LoginRepository.user, DatabaseReference.CompletionListener { error, ref ->
+                        if (error == null) {
+                            database.getReference("users")
+                                .child(LoginRepository.user!!.userId)
+                                .child("data")
+                                .setValue(Json.encodeToString(Data.categoryList))
+                        }
+                    })
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+        })
     }
 
     fun storeOnlineData(successCallback: (Boolean, String) -> Unit) {
-        database.getReference("users")
-            .child(LoginRepository.user!!.userId)
-            .child("data")
-            .addListenerForSingleValueEvent(object: ValueEventListener {
-                override fun onDataChange(snapshot: DataSnapshot) {
 
-                    val readData = snapshot.value.toString()
-                    val stored = Json.encodeToString(Data.categoryList).toString()
-                    if (stored != readData) {
-                        Log.i("TEST", "save data")
-                        Data.saveDataFromString(readData)
-                    }
-                    Log.i("TEST", "callback")
-                    successCallback(true, "")
+        database.getReference("users").child(LoginRepository.user!!.userId).addListenerForSingleValueEvent(object: ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+
+                if (snapshot.exists()) {
+                    database.getReference("users")
+                        .child(LoginRepository.user!!.userId)
+                        .child("data")
+                        .addListenerForSingleValueEvent(object: ValueEventListener {
+                            override fun onDataChange(snapshot: DataSnapshot) {
+
+                                val readData = snapshot.value.toString()
+                                val stored = Json.encodeToString(Data.categoryList).toString()
+                                if (stored != readData) {
+                                    Log.i("TEST", "save data")
+                                    Data.saveDataFromString(readData)
+                                }
+                                Log.i("TEST", "callback")
+                                successCallback(true, "")
+                            }
+
+                            override fun onCancelled(error: DatabaseError) {
+                                successCallback(false, error.message)
+                            }
+
+                        })
+                } else {
+                    successCallback(false, "no online data")
                 }
+            }
 
-                override fun onCancelled(error: DatabaseError) {
-                    successCallback(false, error.message)
-                }
+            override fun onCancelled(error: DatabaseError) {
 
-            })
+            }
+        })
+
+
 
     }
 }
